1d1951891e276491de685c1011b38dbd8e9e0b8b
Doug Wilbourne <dougwilbourne@gmail.com>
2025-10-03 09:39:03 -0400

    added UriFactory.php to enable dependency injection

--- src/psr7/UriFactory.php
+++ src/psr7/UriFactory.php
@@ -2,19 +2,20 @@
 
 namespace pvc\http\psr7;
 
+use Psr\Http\Message\UriFactoryInterface;
 use Psr\Http\Message\UriInterface;
 
 /**
  * standard implementations of Psr-7 compliant Uri objects all seem to suffer
  * the flaw that they are created with a string to parse and there is no parse
- * method in the spec.  So in order to properly inject a Uri, we need a
- * trivial factory.
+ * method in the spec.  So in order to properly inject a Uri, we need the
+ * factory.
  */
-class UriFactory
+class UriFactory implements UriFactoryInterface
 {
-    public function createUri(string $uriString): UriInterface
+    public function createUri(string $uri = ''): UriInterface
     {
-        $guzzleUri = new \GuzzleHttp\Psr7\Uri($uriString);
+        $guzzleUri = new \GuzzleHttp\Psr7\Uri($uri);
         return new Uri($guzzleUri);
     }
 }
