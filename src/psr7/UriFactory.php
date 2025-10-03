b0cd85594d48453327a02732b7ce8be266cc6d83
Doug Wilbourne <dougwilbourne@gmail.com>
2025-10-03 09:40:47 -0400

    added UriFactory.php to enable dependency injection

--- src/psr7/UriFactory.php
+++ src/psr7/UriFactory.php
@@ -8,7 +8,8 @@ use Psr\Http\Message\UriInterface;
 /**
  * standard implementations of Psr-7 compliant Uri objects all seem to suffer
  * the flaw that they are created with a string to parse and there is no parse
- * method in the spec.  So in order to properly inject a Uri, we need the
+ * method in the spec.  This is a byproduct of the design point that Uris should
+ * be immutable.  So in order to properly inject a Uri, we need the
  * factory.
  */
 class UriFactory implements UriFactoryInterface
